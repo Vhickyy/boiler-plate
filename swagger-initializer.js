f7bb35c04fa4e534cc7c53a34b713a0fc3edabe9
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-07-13 22:04:39 +0100

    initial commit

---
+++ swagger-initializer.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const ui = SwaggerUIBundle({
+        url: "./openapi.json",
+        dom_id: '#swagger-ui',
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
+    })
+
+    window.ui = ui
+}
